9f4e9a3aa3b0d017a6966fb77fef60810599a5ad
yusikerem <kerem.yky@gmail.com>
2023-03-28 21:47:15 +0300

    Add insertLetter, deleteLetter

--- src/App.vue
+++ src/App.vue
@@ -5,57 +5,89 @@
     >
       <button><img src="./assets/icons/questionMark.svg" alt="" /></button>
       <h2 class="font text-2xl md:text-4xl font-semibold">WORDLE</h2>
-      <div class="space-x-4 md:space-x-7">
+      <div class="gap-x-4 md:gap-x-7 flex items-center">
         <button><img src="./assets/icons/statistics.svg" alt="" /></button>
         <button><img src="./assets/icons/options.svg" alt="" /></button>
       </div>
     </div>
+
     <div class="mx-auto w-2/3 md:w-1/3 lg:w-1/5">
       <div class="grid grid-cols-5 gap-2 w-full">
         <div
-          class="rounded-md bg-cell_default_bg aspect-square text-3xl md:text-4xl font-extrabold text-white flex items-center justify-center"
-          v-for="cell in 30"
-          :key="cell"
+          class="rounded-md bg-cell_default_bg aspect-square text-3xl uppercase md:text-4xl font-extrabold text-white flex items-center justify-center"
+          v-for="(cell, i) in cellValues"
+          :key="i"
         >
-          2
+          {{ cell }}
         </div>
       </div>
     </div>
     <div class="mx-auto w-max flex flex-col gap-3 items-center">
       <div class="space-x-2 md:space-x-3">
-        <button data-key="q" class="keyboard-btn">Q</button>
-        <button data-key="w" class="keyboard-btn">W</button>
-        <button data-key="e" class="keyboard-btn">E</button>
-        <button data-key="r" class="keyboard-btn">R</button>
-        <button data-key="t" class="keyboard-btn">T</button>
-        <button data-key="y" class="keyboard-btn">Y</button>
-        <button data-key="u" class="keyboard-btn">U</button>
-        <button data-key="i" class="keyboard-btn">I</button>
-        <button data-key="o" class="keyboard-btn">O</button>
-        <button data-key="p" class="keyboard-btn">P</button>
+        <button @click="insertLetter('q')" class="keyboard-btn">Q</button>
+        <button @click="insertLetter('w')" class="keyboard-btn">W</button>
+        <button @click="insertLetter('e')" class="keyboard-btn">E</button>
+        <button @click="insertLetter('r')" class="keyboard-btn">R</button>
+        <button @click="insertLetter('t')" class="keyboard-btn">T</button>
+        <button @click="insertLetter('y')" class="keyboard-btn">Y</button>
+        <button @click="insertLetter('u')" class="keyboard-btn">U</button>
+        <button @click="insertLetter('i')" class="keyboard-btn">I</button>
+        <button @click="insertLetter('o')" class="keyboard-btn">O</button>
+        <button @click="insertLetter('p')" class="keyboard-btn">P</button>
       </div>
       <div class="space-x-2 md:space-x-3">
-        <button data-key="a" class="keyboard-btn">A</button>
-        <button data-key="s" class="keyboard-btn">S</button>
-        <button data-key="d" class="keyboard-btn">D</button>
-        <button data-key="f" class="keyboard-btn">F</button>
-        <button data-key="g" class="keyboard-btn">G</button>
-        <button data-key="h" class="keyboard-btn">H</button>
-        <button data-key="j" class="keyboard-btn">J</button>
-        <button data-key="k" class="keyboard-btn">K</button>
-        <button data-key="l" class="keyboard-btn">L</button>
+        <button @click="insertLetter('a')" class="keyboard-btn">A</button>
+        <button @click="insertLetter('s')" class="keyboard-btn">S</button>
+        <button @click="insertLetter('d')" class="keyboard-btn">D</button>
+        <button @click="insertLetter('f')" class="keyboard-btn">F</button>
+        <button @click="insertLetter('g')" class="keyboard-btn">G</button>
+        <button @click="insertLetter('h')" class="keyboard-btn">H</button>
+        <button @click="insertLetter('j')" class="keyboard-btn">J</button>
+        <button @click="insertLetter('k')" class="keyboard-btn">K</button>
+        <button @click="insertLetter('l')" class="keyboard-btn">L</button>
       </div>
       <div class="space-x-2 md:space-x-3 flex items-center relative">
-        <button data-key="enter" class="keyboard-btn md:px-5">ENTER</button>
-        <button data-key="z" class="keyboard-btn">Z</button>
-        <button data-key="x" class="keyboard-btn">X</button>
-        <button data-key="c" class="keyboard-btn">C</button>
-        <button data-key="v" class="keyboard-btn">V</button>
-        <button data-key="b" class="keyboard-btn">B</button>
-        <button data-key="n" class="keyboard-btn">N</button>
-        <button data-key="m" class="keyboard-btn">M</button>
-        <button data-key="delete" class="keyboard-btn h-full">DEL</button>
+        <button @click="predict()" class="keyboard-btn md:px-5">ENTER</button>
+        <button @click="insertLetter('z')" class="keyboard-btn">Z</button>
+        <button @click="insertLetter('x')" class="keyboard-btn">X</button>
+        <button @click="insertLetter('c')" class="keyboard-btn">C</button>
+        <button @click="insertLetter('v')" class="keyboard-btn">V</button>
+        <button @click="insertLetter('b')" class="keyboard-btn">B</button>
+        <button @click="insertLetter('n')" class="keyboard-btn">N</button>
+        <button @click="insertLetter('m')" class="keyboard-btn">M</button>
+        <button @click="deleteLetter()" class="keyboard-btn h-full">DEL</button>
       </div>
     </div>
   </section>
 </template>
+<script setup lang="ts">
+import { ref, computed } from "vue";
+const entries = ref(
+  Array.from({ length: 6 }, () => Array.from({ length: 5 }, () => ""))
+);
+let index = 0;
+let row = ref(0);
+
+function insertLetter(key: string) {
+  index <= 4
+    ? (entries.value[row.value].splice(index, 1, key), index++)
+    : undefined;
+}
+function deleteLetter() {
+  entries.value[row.value].splice(index - 1, 1, "");
+  index > 0 ? index-- : undefined;
+}
+function predict() {
+  console.log("sa");
+  entries.value[row.value].filter((cell) => cell != "").length == 5
+    ? (alert("check"), row.value++, (index = 0))
+    : alert("Not enough letters");
+}
+const cellValues = computed(() => {
+  let arr: string[] = [];
+  entries.value.forEach((element) => {
+    element.forEach((x) => arr.push(x));
+  });
+  return arr;
+});
+</script>
